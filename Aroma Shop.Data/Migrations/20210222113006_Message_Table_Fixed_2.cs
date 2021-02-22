ea6d4636d06add35b410b6e646f07f14d2225ef0
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-22 15:00:37 +0330

    Add Message Model To DB

---
+++ Aroma Shop.Data/Migrations/20210222113006_Message_Table_Fixed_2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aroma_Shop.Data.Migrations
+{
+    public partial class Message_Table_Fixed_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageSubject",
+                table: "Messages",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageSenderName",
+                table: "Messages",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(75)",
+                oldMaxLength: 75);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageSubject",
+                table: "Messages",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageSenderName",
+                table: "Messages",
+                type: "nvarchar(75)",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 150);
+        }
+    }
+}
--- Aroma Shop.Domain/Models/Message.cs
+++ Aroma Shop.Domain/Models/Message.cs
@@ -9,14 +9,14 @@ namespace Aroma_Shop.Domain.Models
     {
         [Key]
         public int MessageId { get; set; }
-        [MaxLength(75,ErrorMessage = "حداکثر 75 کارکتر مجاز می باشد")]
+        [MaxLength(150,ErrorMessage = "حداکثر 150 کارکتر مجاز می باشد")]
         [Required(ErrorMessage = "لطفا نام خود را وارد نمایید")]
         public string MessageSenderName { get; set; }
         [EmailAddress(ErrorMessage = "فیلد وارد شده ایمیل نمی باید")]
         [MaxLength(200, ErrorMessage = "حداکثر 200 کارکتر مجاز می باشد")]
         [Required(ErrorMessage = "لطفا آدرس ایمیل خود را وارد نمایید")]
         public string MessageSenderEmail { get; set; }
-        [MaxLength(100, ErrorMessage = "حداکثر 100 کارکتر مجاز می باشد")]
+        [MaxLength(200, ErrorMessage = "حداکثر 200 کارکتر مجاز می باشد")]
         [Required(ErrorMessage = "لطفا موضوع پیام خود را وارد نمایید")]
         public string MessageSubject { get; set; }
         [Required(ErrorMessage = "لطفا پیام خود را شرح دهید")]
