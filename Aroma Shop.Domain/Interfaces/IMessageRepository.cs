cbf5e0b913ef5f24288f43b1fe4fc2be0ddde64e
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-22 14:51:02 +0330

    Add IMessageRepository In Domain

---
+++ Aroma Shop.Domain/Interfaces/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aroma_Shop.Domain.Models;
+
+namespace Aroma_Shop.Domain.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+    }
+}
