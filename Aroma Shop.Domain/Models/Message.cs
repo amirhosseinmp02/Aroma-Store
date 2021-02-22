47ce798a663ee9fd9c583de6d5e31e32da34da41
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-22 14:33:55 +0330

    Create Message Model

---
+++ Aroma Shop.Domain/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aroma_Shop.Domain.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageSenderName { get; set; }
+        public string MessageSenderEmail { get; set; }
+        public string MessageSubject { get; set; }
+        public string MessageDescription { get; set; }
+    }
+}
