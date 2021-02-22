5a24b95056565e69af53d5b72cbb9cfca9159928
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-22 14:48:06 +0330

    Create Message Model

--- Aroma Shop.Domain/Models/Message.cs
+++ Aroma Shop.Domain/Models/Message.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Aroma_Shop.Domain.Models
 {
     public class Message
     {
+        [Key]
         public int MessageId { get; set; }
+        [MaxLength(75,ErrorMessage = "حداکثر 75 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "لطفا نام خود را وارد نمایید")]
         public string MessageSenderName { get; set; }
+        [EmailAddress(ErrorMessage = "فیلد وارد شده ایمیل نمی باید")]
+        [Required(ErrorMessage = "لطفا آدرس ایمیل خود را وارد نمایید")]
         public string MessageSenderEmail { get; set; }
+        [MaxLength(100, ErrorMessage = "حداکثر 100 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "لطفا موضوع پیام خود را وارد نمایید")]
         public string MessageSubject { get; set; }
+        [Required(ErrorMessage = "لطفا پیام خود را شرح دهید")]
         public string MessageDescription { get; set; }
     }
 }
