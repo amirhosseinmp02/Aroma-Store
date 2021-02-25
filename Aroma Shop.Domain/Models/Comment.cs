04506f268726b736f6aa93e3b6ae78783a5ac89a
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-26 03:08:42 +0330

    Comment Model Created

---
+++ Aroma Shop.Domain/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Aroma_Shop.Domain.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [MaxLength(100, ErrorMessage = "حداکثر 150 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "لطفا نام خود را وارد نمایید")]
+        public string CommentName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string CommentEmail { get; set; }
+        [Required(ErrorMessage = "لطفا نظر خود را تکمیل کنید")]
+        [MaxLength(10000, ErrorMessage = "حداکثر 10000 کارکتر مجاز می باشد")]
+        public string CommentMessage { get; set; }
+    }
+}
