2aaf06448a3a645dcfb3ac9115f79f34d2379562
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-26 03:13:05 +0330

    Image Model Created

---
+++ Aroma Shop.Domain/Models/Image.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Aroma_Shop.Domain.Models
+{
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+        [Required]
+        [MaxLength(300)]
+        [Display(Name = "آپلود تصویر")]
+        public string ImageName { get; set; }
+        [NotMapped]
+        [Display(Name = "آپلود تصویر")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
