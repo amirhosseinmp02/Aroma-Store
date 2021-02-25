b7682349037c0cdcff9799d07c7396f5f96993d0
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-25 23:58:18 +0330

    ProductInformation Model Created

---
+++ Aroma Shop.Domain/Models/ProductInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Aroma_Shop.Domain.Models
+{
+    public class ProductInformation
+    {
+        [Key]
+        public int ProductInformationId { get; set; }
+        [MaxLength(100, ErrorMessage = "حداکثر 100 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "لطفا نام مشخصه را وارد نمایید")]
+        public string Name { get; set; }
+        [MaxLength(250, ErrorMessage = "حداکثر 250 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "لطفا مقدار مشخصه را وارد نمایید")]
+        public string Value { get; set; }
+    }
+}
