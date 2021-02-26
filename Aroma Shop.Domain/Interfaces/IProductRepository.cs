7059f08462989c5bb5509c324ec75817d930af16
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-26 15:00:44 +0330

    IProductRepository Added

---
+++ Aroma Shop.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aroma_Shop.Domain.Models;
+
+namespace Aroma_Shop.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct();
+    }
+}
