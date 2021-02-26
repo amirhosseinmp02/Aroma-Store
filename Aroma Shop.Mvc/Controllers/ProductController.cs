09460d7aab967f4881bfa73100a3ff179ac6de7f
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-26 20:33:26 +0330

    ProductController Added ProductDetails Action Added

---
+++ Aroma Shop.Mvc/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aroma_Shop.Application.Interfaces;
+
+namespace Aroma_Shop.Mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [Route("/Products/{productId}")]
+        public IActionResult ProductDetails(int productId)
+        {
+            var model = _productService.GetProduct(productId);
+            return View();
+        }
+    }
+}
