b98146a6bbffc870e04fc34ff33b26c42f261ae2
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-28 01:54:45 +0330

    AccountController Created

---
+++ Aroma Shop.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aroma_Shop.Application.Interfaces;
+
+namespace Aroma_Shop.Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+
+    }
+}
