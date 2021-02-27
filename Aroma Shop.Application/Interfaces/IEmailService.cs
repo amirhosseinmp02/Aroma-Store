f3f4e5dd570b687a635ae7def2599a8b3c6a89d8
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-27 19:02:51 +0330

    IEmailService Created

---
+++ Aroma Shop.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aroma_Shop.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        public bool SendEmailAsync(string toEmail, string subject, string message, bool isMessageHtml = false);
+    }
+}
