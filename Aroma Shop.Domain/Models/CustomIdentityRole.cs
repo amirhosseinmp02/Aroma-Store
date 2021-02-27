8dd618cd585f31c8cfaaddcfc4b6476856016322
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-27 16:34:32 +0330

    CustomIdentityRole Created

---
+++ Aroma Shop.Domain/Models/CustomIdentityRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Aroma_Shop.Domain.Models
+{
+    class CustomIdentityRole : IdentityRole
+    {
+        public int Rank { get; set; }
+
+        public CustomIdentityRole()
+        {
+
+        }
+        public CustomIdentityRole(string roleName, int rank) : base(roleName)
+        {
+            Rank = rank;
+        }
+    }
+}
