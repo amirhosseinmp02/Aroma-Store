819b6d8ccb66a0ec23d67b2978c3cc79c405d63b
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-27 16:33:37 +0330

    CustomIdentityUser Created

---
+++ Aroma Shop.Domain/Models/CustomIdentityUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Aroma_Shop.Domain.Models
+{
+    public class CustomIdentityUser : IdentityUser
+    {
+
+    }
+}
