5d0c915ef5c94160bcaf06899db10c6bb09ee8fa
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-28 02:24:06 +0330

    RegisterViewModel Created

---
+++ Aroma Shop.Application/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aroma_Shop.Application.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "لطفا نام کاربری مورد نظر را وارد نمایید")]
+        [MaxLength(75, ErrorMessage = "حداکثر 75 کارکتر مجاز می باشد")]
+        [Remote("IsUserNameExist", "Account", HttpMethod = "POST"
+            , AdditionalFields = "__RequestVerificationToken")]
+        public string UserName { get; set; }
+        [EmailAddress(ErrorMessage = "فیلد وارد شده ایمیل نمی باشد")]
+        [Required(ErrorMessage = "لطفا آدرس ایمیل خود را وارد نمایید")]
+        [Remote("IsEmailExist", "Account", HttpMethod = "POST"
+            , AdditionalFields = "__RequestVerificationToken")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا کلمه عبور مورد نظر را وارد نمایید")]
+        [DataType(DataType.Password)]
+        [MaxLength(16, ErrorMessage = "حداکثر 16 کارکتر مجاز می باشد")]
+        [MinLength(8, ErrorMessage = "حداکثر 8 کارکتر مجاز می باشد")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "لطفا تکرار کلمه عبور را وارد نمایید")]
+        [Compare(nameof(Password),ErrorMessage = "تکرار کلمه عبور با کلمه عبور مطابقت ندارد")]
+        public string RePassword { get; set; }
+    }
+}
