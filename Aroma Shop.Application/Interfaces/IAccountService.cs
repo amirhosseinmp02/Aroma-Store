8f155271b769e9e89adee0669563d83a6cc75749
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-27 16:19:13 +0330

    IAccountService Created

---
+++ Aroma Shop.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Aroma_Shop.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        bool IsUserSignedIn(ClaimsPrincipal user);
+    }
+}
