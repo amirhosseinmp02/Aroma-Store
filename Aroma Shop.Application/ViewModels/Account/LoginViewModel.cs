aababb9957b2e63c6c9cf00b8e67ad86a02fb538
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-02-28 17:57:20 +0330

    LoginViewModel Created

---
+++ Aroma Shop.Application/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aroma_Shop.Application.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+        [EmailAddress(ErrorMessage = "فیلد وارد شده ایمیل نمی باشد")]
+        [Required(ErrorMessage = "لطفا آدرس ایمیل خود را وارد نمایید")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا کلمه عبور خود را وارد نمایید")]
+        [DataType(DataType.Password)]
+        [MaxLength(16, ErrorMessage = "حداکثر 16 کارکتر مجاز می باشد")]
+        [MinLength(8, ErrorMessage = "حداقل 8 کارکتر مجاز می باشد")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+        public List<AuthenticationScheme> ExternalsLogins { get; set; }
+    }
+}
