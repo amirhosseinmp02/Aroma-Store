730068b67b1ec8664874a34c3317ed792da7ea60
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-01 22:29:33 +0330

    Slide Drop Down Menu Created

--- Aroma Shop.Mvc/wwwroot/js/admin/dashboard.js
+++ Aroma Shop.Mvc/wwwroot/js/admin/dashboard.js
@@ -58,7 +58,6 @@ var i;
 
 for (i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function () {
-        this.classList.toggle("active");
         var dropdownContent = this.nextElementSibling;
         if (dropdownContent.style.display === "block") {
             dropdownContent.style.display = "none";
