6f92cf941d942ddc738af1a8b1a8a696e72cbcaf
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-02 01:24:32 +0330

    Add CategoryDescription Field To Category Model,Db

---
+++ Aroma Shop.Data/Migrations/20210301215309_add_CategoryDescription.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aroma_Shop.Data.Migrations
+{
+    public partial class add_CategoryDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageSenderEmail",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryDescription",
+                table: "Categories",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryDescription",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MessageSenderEmail",
+                table: "Messages",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Aroma Shop.Domain/Models/ProductModels/Category.cs
+++ Aroma Shop.Domain/Models/ProductModels/Category.cs
@@ -18,6 +18,8 @@ namespace Aroma_Shop.Domain.Models.ProductModels
         [MaxLength(100, ErrorMessage = "حداکثر 100 کارکتر مجاز می باشد")]
         [Required(ErrorMessage = "لطفا نام دسته را وارد نمایید")]
         public string CategoryName { get; set; }
+        [MaxLength(1000, ErrorMessage = "حداکثر 1000 کارکتر مجاز می باشد")]
+        public string CategoryDescription { get; set; }
 
         //Navigations Proterties
 
