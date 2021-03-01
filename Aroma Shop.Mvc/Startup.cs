adc91159d2964e6334655a0faa803a4a04990641
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-01 16:46:28 +0330

    Area Admin Created

--- Aroma Shop.Mvc/Startup.cs
+++ Aroma Shop.Mvc/Startup.cs
@@ -88,6 +88,10 @@ namespace Aroma_Shop.Mvc
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
