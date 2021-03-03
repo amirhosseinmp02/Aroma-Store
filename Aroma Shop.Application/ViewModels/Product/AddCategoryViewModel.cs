d17533522ce6b9425a4bcde9107b9365f2536366
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-03 16:42:01 +0330

    Add ParentCategoryId To AddCategoryViewModel

--- Aroma Shop.Application/ViewModels/Product/AddCategoryViewModel.cs
+++ Aroma Shop.Application/ViewModels/Product/AddCategoryViewModel.cs
@@ -14,6 +14,7 @@ namespace Aroma_Shop.Application.ViewModels.Product
         public string CategoryName { get; set; }
         [MaxLength(1000, ErrorMessage = "حداکثر 1000 کارکتر مجاز می باشد")]
         public string CategoryDescription { get; set; }
+        public int ParentCategoryId { get; set; }   
         public IEnumerable<SelectListItem> AllCategories { get; set; }
     }
 }
