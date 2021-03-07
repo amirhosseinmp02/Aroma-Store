52762dbe28b1b44d2b38488c8a2bc8d8ddd5ce34
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-07 16:44:36 +0330

    AddEditProductViewModel Created

---
+++ Aroma Shop.Application/ViewModels/Product/AddEditProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Aroma_Shop.Application.ViewModels.Product
+{
+    public class AddEditProductViewModel
+    {
+        [MaxLength(200, ErrorMessage = "حداکثر 200 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "لطفا نام محصول را وارد نمایید")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "لطفا قیمت محصول را وارد نمایید")]
+        public double ProductPrice { get; set; }
+        [Required(ErrorMessage = "لطفا تعداد محصول را وارد نمایید")]
+        public int ProductQuantityInStock { get; set; }
+        [MaxLength(10000, ErrorMessage = "حداکثر 10000 کارکتر مجاز می باشد")]
+        public string ProductDescription { get; set; }
+
+        public IFormCollection Images { get; set; }
+
+    }
+}
