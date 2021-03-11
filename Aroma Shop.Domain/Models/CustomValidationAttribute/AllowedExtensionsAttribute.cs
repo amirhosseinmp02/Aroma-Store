f8d80a85da10b81b641665f5c7c053ea6a197485
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-11 05:17:48 +0330

    AllowedExtensionsAttribute Created

---
+++ Aroma Shop.Domain/Models/CustomValidationAttribute/AllowedExtensionsAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Aroma_Shop.Domain.Models.CustomValidationAttribute
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!((IList) _extensions).Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"این نوع پسوند برای فایل مورد نظر پشتیبانی نمی شود";
+        }
+    }
+}
