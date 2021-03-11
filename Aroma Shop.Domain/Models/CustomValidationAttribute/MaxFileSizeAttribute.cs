96b6b09294b7de13078a75b1e55a9a8bbeb62692
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-03-11 05:15:16 +0330

    MaxFileSizeAttribute Created

---
+++ Aroma Shop.Domain/Models/CustomValidationAttribute/MaxFileSizeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Aroma_Shop.Domain.Models.CustomValidationAttribute
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > _maxFileSize)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"حداکثر حجم مجاز برای هر فایل { _maxFileSize} می باشد";
+        }
+    }
+}
