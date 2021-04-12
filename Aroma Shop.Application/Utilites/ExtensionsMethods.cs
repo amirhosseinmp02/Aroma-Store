33624eeb42f6191711000162c85002b0a2753537
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-04-13 00:15:01 +0430

    ToSolar And ToSolarWithTime ExtensionsMethods Created

--- Aroma Shop.Application/Utilites/ExtensionsMethods.cs
+++ Aroma Shop.Application/Utilites/ExtensionsMethods.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Aroma_Shop.Application.Utilites
 {
     public static class ExtensionsMethods
     {
+        public static string ToSolar(this DateTime dateTime)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
 
+            var solarCalendar =
+                $"{persianCalendar.GetYear(dateTime)}/{persianCalendar.GetMonth(dateTime)}" +
+                $"/{persianCalendar.GetDayOfMonth(dateTime)}";
+
+            return solarCalendar;
+        }
+
+        public static string ToSolarWithTime(this DateTime dateTime)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+
+            var solarCalendar =
+                $"{persianCalendar.GetYear(dateTime)}/{persianCalendar.GetMonth(dateTime)}" +
+                $"/{persianCalendar.GetDayOfMonth(dateTime)} - " +
+                $"{persianCalendar.GetHour(dateTime)}:{persianCalendar.GetMinute(dateTime)}";
+
+            return solarCalendar;
+        }
     }
 }
