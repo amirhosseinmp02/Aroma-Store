6399e625a947940df2608394d0f2502037cbc670
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-16 21:29:05 +0430

    PolicyRequirement , PolicyHander Created For Set Roles , And Policy Settings . I Add Founder , Manager , Writer , Customer Policy Rank For Giving Authority

---
+++ Aroma Shop.Application/Security/Policy/PolicyHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Aroma_Shop.Application.Security.Policy
+{
+    public class PolicyHandler : AuthorizationHandler<PolicyRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PolicyRequirement requirement)
+        {
+            if ((requirement.PolicyName == "Founder") && (context.User.IsInRole("Founder")))
+            {
+                context.Succeed(requirement);
+            }
+
+            else if ((requirement.PolicyName == "Manager") && (context.User.IsInRole("Manager") || context.User.IsInRole("Founder")))
+            {
+                context.Succeed(requirement);
+            }
+
+            else if ((requirement.PolicyName == "Writer") && (context.User.IsInRole("Writer") || context.User.IsInRole("Manager") || context.User.IsInRole("Founder")))
+            {
+                context.Succeed(requirement);
+            }
+
+            else if ((requirement.PolicyName == "Customer") && (context.User.IsInRole("Customer") ||
+                                                                context.User.IsInRole("Writer") ||
+                                                                context.User.IsInRole("Manager") ||
+                                                                context.User.IsInRole("Founder")))
+            {
+
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
