85dc7cc6ef84bb8425f122ff55bdeab93bb577d6
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-17 16:04:00 +0430

    UserViewModel Created For Users Index

---
+++ Aroma Shop.Application/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aroma_Shop.Application.ViewModels.User
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string PersianUserRoleName { get; set; }
+    }
+}
