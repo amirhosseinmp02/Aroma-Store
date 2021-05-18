dbea1233ce4eda2cead6565ded0d73760e1e9dff
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-18 14:00:48 +0430

    UserDetail Created

---
+++ Aroma Shop.Domain/Models/UserModels/UserDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Aroma_Shop.Domain.Models.UserModels
+{
+    public class UserDetail
+    {
+        [MaxLength(150, ErrorMessage = "حداکثر 150 کارکتر مجاز می باشد")]
+        public string FirstName { get; set; }
+        [MaxLength(150, ErrorMessage = "حداکثر 150 کارکتر مجاز می باشد")]
+        public string LastName { get; set; }
+        [MaxLength(200, ErrorMessage = "حداکثر 200 کارکتر مجاز می باشد")]
+        public string UserProvince { get; set; }
+        [MaxLength(150, ErrorMessage = "حداکثر 150 کارکتر مجاز می باشد")]
+        public string UserCity { get; set; }
+        public string UserAddress { get; set; }
+        [StringLength(10,MinimumLength = 10,ErrorMessage = "کد پستی شامل 10 رقم می باشد")]
+        public string UserZipCode { get; set; }
+
+    }
+}
