2cfecf504e1ca24110842a4ad757b986f5cef070
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-22 17:33:13 +0430

    Migration For Create Relation Between CustomIdentityUser And Comment Was Added And The DataBase Updated

---
+++ Aroma Shop.Data/Migrations/20210522125640_SetOneToManyRelationBetweenCustomIdentityUserModelAndCommentModel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aroma_Shop.Data.Migrations
+{
+    public partial class SetOneToManyRelationBetweenCustomIdentityUserModelAndCommentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentEmail",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CommentName",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CommentEmail",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CommentName",
+                table: "Comments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
