bcf1164f201173e367e5b11ec4ee1755fcdb56bc
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-23 12:50:38 +0430

    Add IsRead Property To Comment Model , It's Migration Was Created And The DataBase Updated

---
+++ Aroma Shop.Data/Migrations/20210523081642_AddIsReadProperyToCommentModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aroma_Shop.Data.Migrations
+{
+    public partial class AddIsReadProperyToCommentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Comments");
+        }
+    }
+}
