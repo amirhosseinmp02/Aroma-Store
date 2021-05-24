37b6a73f9e07824a7c1522d021e3337ded821ac3
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-24 15:05:09 +0430

    Show Only The Confirmed Comments And Replies In ProductDetail

--- Aroma Shop.Application/ViewModels/Product/ProductViewModel.cs
+++ Aroma Shop.Application/ViewModels/Product/ProductViewModel.cs
@@ -8,7 +8,7 @@ namespace Aroma_Shop.Application.ViewModels.Product
     public class ProductViewModel
     {
         public Domain.Models.ProductModels.Product Product { get; set; }
-        [Required(ErrorMessage = "لطفا دیدگاه / پاسخ خود را وارد کنید")]
+        [Required(ErrorMessage = "لطفا دیدگاه / پاسخ خود را وارد کنید.")]
         [MaxLength(10000, ErrorMessage = "حداکثر 10000 کارکتر مجاز می باشد")]
         public string CommentDescription { get; set; }
 
