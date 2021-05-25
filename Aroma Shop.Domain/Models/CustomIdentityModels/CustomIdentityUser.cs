a609740f875c40dd69b13e9604bad6e30e9fadd7
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-05-25 12:44:12 +0430

    Add RegisterTime Property To CustomIdentityUser

--- Aroma Shop.Domain/Models/CustomIdentityModels/CustomIdentityUser.cs
+++ Aroma Shop.Domain/Models/CustomIdentityModels/CustomIdentityUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Aroma_Shop.Domain.Models.ProductModels;
 using Aroma_Shop.Domain.Models.UserModels;
@@ -9,6 +10,8 @@ namespace Aroma_Shop.Domain.Models.CustomIdentityModels
 {
     public class CustomIdentityUser : IdentityUser
     {
+        [Required]
+        public DateTime RegisterTime { get; set; }  
 
         //Navigations Properties
 
