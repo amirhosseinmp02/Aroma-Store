ef7675a1eaad1bfcc9dc8e8c1bc66bcfb997a0dc
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-06-09 18:24:30 +0430

    Admin Area / Page Controller Index GetAction Was Created For Show Pages

--- Aroma Shop.Mvc/Areas/Admin/Controllers/PageController.cs
+++ Aroma Shop.Mvc/Areas/Admin/Controllers/PageController.cs
@@ -3,18 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aroma_Shop.Application.Interfaces;
 
 namespace Aroma_Shop.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PageController : Controller
     {
+        private readonly IPageService _pageService;
+
+        public PageController(IPageService pageService)
+        {
+            _pageService = pageService;
+        }
+
         #region ShowPages
 
         [HttpGet("/Admin/Pages")]
         public IActionResult Index()
         {
-            return View();
+            var pages =
+                _pageService.GetPages();
+
+            return View(pages);
         }
 
         #endregion
