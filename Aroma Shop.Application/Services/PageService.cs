48b68fd9dd7f7d1dd69b4091016862417ee83b7f
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-06-09 18:13:46 +0430

    PageService Was Created AddPage Was Implemented In PageService

---
+++ Aroma Shop.Application/Services/PageService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aroma_Shop.Application.Interfaces;
+using Aroma_Shop.Domain.Interfaces;
+using Aroma_Shop.Domain.Models.PageModels;
+
+namespace Aroma_Shop.Application.Services
+{
+    public class PageService : IPageService
+    {
+        private readonly IPageRepository _pageRepository;
+
+        public PageService(IPageRepository pageRepository)
+        {
+            _pageRepository = pageRepository;
+        }
+
+        public bool AddPage(Page page)
+        {
+            try
+            {
+                _pageRepository.AddPage(page);
+
+                return true;
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+                return false;
+            }
+        }
+    }
+}
