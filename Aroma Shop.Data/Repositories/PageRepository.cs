ba742dcce5e9df55075860fe89cc3e166340ec17
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-06-09 17:57:48 +0430

    PageRepository Was Created AddPage , Save Was Implemented In PageRepository

---
+++ Aroma Shop.Data/Repositories/PageRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aroma_Shop.Data.Context;
+using Aroma_Shop.Domain.Interfaces;
+using Aroma_Shop.Domain.Models.PageModels;
+
+namespace Aroma_Shop.Data.Repositories
+{
+    public class PageRepository : IPageRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddPage(Page page)
+        {
+            _context.Add(page);
+        }
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
