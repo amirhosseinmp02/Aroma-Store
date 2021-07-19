c6e054e18d60f64002d7939354905853bfa22eba
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-20 00:59:10 +0430

    Admin Area / Order Controller Was Created

---
+++ Aroma Shop.Mvc/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aroma_Shop.Mvc.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        
+    }
+}
