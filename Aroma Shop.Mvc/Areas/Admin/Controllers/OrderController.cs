d0337bd649d150e50d43f3a7519682e221f267ee
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-20 01:09:53 +0430

    Admin Area / Order Controller / AddDiscount View Was Created Admin Area / Order Controller / AddDiscount HttpPost Action Was Created

--- Aroma Shop.Mvc/Areas/Admin/Controllers/OrderController.cs
+++ Aroma Shop.Mvc/Areas/Admin/Controllers/OrderController.cs
@@ -3,13 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aroma_Shop.Domain.Models.ProductModels;
 
 namespace Aroma_Shop.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class OrderController : Controller
     {
+        [HttpGet("/Admin/Orders/AddDiscount")]
         public IActionResult AddDiscount()
+        {
+            return View();
+        }
+
+        [HttpPost("/Admin/Orders/AddDiscount")]
+        public IActionResult AddDiscount(Discount model)
         {
             return View();
         }
