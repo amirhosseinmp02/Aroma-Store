37eeb4de34c84c0ef3d94a62dc41c107acfb34cb
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-20 17:05:03 +0430

    Discount Section Was Added To Admin Dashbord Menu

--- Aroma Shop.Mvc/Areas/Admin/Controllers/OrderController.cs
+++ Aroma Shop.Mvc/Areas/Admin/Controllers/OrderController.cs
@@ -7,9 +7,11 @@ using Aroma_Shop.Application.Interfaces;
 using Aroma_Shop.Application.Utilites;
 using Aroma_Shop.Application.ViewModels.Product;
 using Aroma_Shop.Domain.Models.ProductModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Aroma_Shop.Mvc.Areas.Admin.Controllers
 {
+    [Authorize(Policy = "Writer")]
     [Area("Admin")]
     public class OrderController : Controller
     {
