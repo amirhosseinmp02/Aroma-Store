b86cb12f3ce0702ff1d992b6d53e9efb2709f2a6
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-22 03:12:37 +0430

    Some Styles Were Added

--- Aroma Shop.Application/ViewModels/Product/CartCheckOutViewModel.cs
+++ Aroma Shop.Application/ViewModels/Product/CartCheckOutViewModel.cs
@@ -17,15 +17,19 @@ namespace Aroma_Shop.Application.ViewModels.Product
         public string LastName { get; set; }
         [DataType(DataType.PhoneNumber, ErrorMessage = "شماره موبایل وارد شده معتبر نیست")]
         [StringLength(11, MinimumLength = 11, ErrorMessage = "شماره موبایل وارد شده معتبر نیست")]
+        [Required(ErrorMessage = "شماره موبایل را وارد نمایید")]
         public string MobileNumber { get; set; }
         [MaxLength(250, ErrorMessage = "حداکثر 250 کارکتر مجاز می باشد")]
+        [Required(ErrorMessage = "استان خود را انتخاب نمایید")]
         public string UserProvince { get; set; }
         [MaxLength(250, ErrorMessage = "حداکثر 250 کارکتر مجاز می باشد")]
-        [RequiredIfNotNull(nameof(UserProvince), ErrorMessage = "در صورت انتخاب استان ، شهر خود را هم انتخاب نمایید")]
+        [Required(ErrorMessage = "شهر خود را انتخاب نمایید")]
         public string UserCity { get; set; }
+        [Required(ErrorMessage = "آدرس خود را وارد نمایید")]
         public string UserAddress { get; set; }
         [StringLength(10, MinimumLength = 10, ErrorMessage = "کد پستی شامل 10 رقم می باشد")]
         [RegularExpression("([0-9]+)", ErrorMessage = "کد پستی تنها میتواند شامل عدد باشد")]
+        [Required(ErrorMessage = "کد پستی خود را وارد نمایید")]
         public string UserZipCode { get; set; }
         public string OrderNote { get; set; }
 
