998a7c872c335910589955f0227bba2bfe463164
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-24 01:14:22 +0430

    OrderTrackingViewModel Was Created

---
+++ Aroma Shop.Application/ViewModels/Product/OrderTrackingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Aroma_Shop.Application.ViewModels.Product
+{
+    public class OrderTrackingViewModel
+    {
+        [Required(ErrorMessage = "لطفا شناسه سفارش خود را وارد نمایید")]
+        public int OrderId { get; set; }
+        [EmailAddress(ErrorMessage = "فیلد وارد شده ایمیل نمی باشد")]
+        [Required(ErrorMessage = "لطفا ایمیل خود را وارد نمایید")]
+        public string Email { get; set; }
+    }
+}
