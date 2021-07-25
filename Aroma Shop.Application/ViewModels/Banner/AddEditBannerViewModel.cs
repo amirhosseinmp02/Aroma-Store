3d6981ffb56cdbb0b62cbe95f0e8ff507c3332c5
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-26 01:24:28 +0430

    AddEditBannerViewModel Was Created

---
+++ Aroma Shop.Application/ViewModels/Banner/AddEditBannerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Aroma_Shop.Application.ViewModels.Banner
+{
+    public class AddEditBannerViewModel
+    {
+        [MaxLength(200)]
+        public string BannerTitle { get; set; }
+        public string BannerDescription { get; set; }
+
+        [Required]
+        public IFormFile BannerImage { get; set; }  
+    }
+}
