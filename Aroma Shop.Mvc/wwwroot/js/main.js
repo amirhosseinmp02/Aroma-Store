2f65546720f55044e143f6961b1f054860acfdae
amirhosseinmp02@gmail.com <amirhosseinmp02@gmail.com>
2021-07-30 20:53:36 +0430

    Some Codes Were Cleaner And Optimized

--- Aroma Shop.Mvc/wwwroot/js/main.js
+++ Aroma Shop.Mvc/wwwroot/js/main.js
@@ -1,53 +1,115 @@
-$(function() {
-  "use strict";
+$(function () {
+    "use strict";
 
-  //------- Parallax -------//
-  skrollr.init({
-    forceHeight: false
-  });
+    //------- Parallax -------//
+    skrollr.init({
+        forceHeight: false
+    });
 
-  //------- fixed navbar --------//  
-  $(window).scroll(function(){
-    var sticky = $('.header_area'),
-    scroll = $(window).scrollTop();
-
-    if (scroll >= 100) sticky.addClass('fixed');
-    else sticky.removeClass('fixed');
-  });
-
-  //------- Price Range slider -------//
-  if(document.getElementById("price-range")){
-  
-    var nonLinearSlider = document.getElementById('price-range');
-    
-    noUiSlider.create(nonLinearSlider, {
-        connect: true,
-        behaviour: 'tap',
-        start: [ 500, 4000 ],
-        range: {
-            // Starting at 500, step the value by 500,
-            // until 4000 is reached. From there, step by 1000.
-            'min': [ 0 ],
-            '10%': [ 500, 500 ],
-            '50%': [ 4000, 1000 ],
-            'max': [ 10000 ]
+    //------- hero carousel -------//
+    $(".hero-carousel").owlCarousel({
+        rtl: true,
+        items: 3,
+        margin: 10,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        loop: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            810: {
+                items: 3
+            }
         }
     });
-  
-  
-    var nodes = [
-        document.getElementById('lower-value'), // 0
-        document.getElementById('upper-value')  // 1
-    ];
-  
-    // Display the slider value and how far the handle moved
-    // from the left edge of the slider.
-    nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
-        nodes[handle].innerHTML = values[handle];
+
+    //------- Best Seller Carousel -------//
+    if ($('.owl-carousel').length > 0) {
+        $('#bestSellerCarousel').owlCarousel({
+            rtl: true,
+            loop: true,
+            margin: 30,
+            nav: true,
+            navText: ["<i class='ti-arrow-left'></i>", "<i class='ti-arrow-right'></i>"],
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    center: true
+                },
+                770: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                },
+                1198: {
+                    items: 4
+                }
+            }
+        });
+    }
+
+    //------- single product area carousel -------//
+    $(".s_Product_carousel").owlCarousel({
+        rtl: true,
+        items: 1,
+        margin:5,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        loop: true,
+        nav: true,
+        dots: false
     });
-  
-  }
-  
+
+    //------- fixed navbar --------//  
+    $(window).scroll(function () {
+        var sticky = $('.header_area'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+    });
+
+    //------- Price Range slider -------//
+    if (document.getElementById("price-range")) {
+
+        var nonLinearSlider = document.getElementById('price-range');
+
+        noUiSlider.create(nonLinearSlider, {
+            connect: true,
+            behaviour: 'tap',
+            start: [500, 4000],
+            range: {
+                // Starting at 500, step the value by 500,
+                // until 4000 is reached. From there, step by 1000.
+                'min': [0],
+                '10%': [500, 500],
+                '50%': [4000, 1000],
+                'max': [10000]
+            }
+        });
+
+
+        var nodes = [
+            document.getElementById('lower-value'), // 0
+            document.getElementById('upper-value')  // 1
+        ];
+
+        // Display the slider value and how far the handle moved
+        // from the left edge of the slider.
+        nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
+            nodes[handle].innerHTML = values[handle];
+        });
+
+    }
+
 });
 
 
